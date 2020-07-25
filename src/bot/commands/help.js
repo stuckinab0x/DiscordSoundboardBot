b32072ad937e727a2467af14adfaaf841196470e
Dave Pringle <3244206+UncleDave@users.noreply.github.com>
2020-07-25 17:35:37 +0100

    Improve formatting of help command

--- src/bot/commands/help.js
+++ src/bot/commands/help.js
@@ -2,30 +2,27 @@ const Command = require('./command');
 const Discord = require('discord.js');
 const constants = require('../constants');
 
-const ZERO_WIDTH_SPACE = '\u200B';
-
 module.exports = function helpConstructor(commands) {
   let helpMessage;
+
   const helpCommand = new Command('help', `${ constants.messagePrefix } help`, 'Display the available commands', message => message.reply(helpMessage));
-  const firstCommand = commands[0];
-  const otherCommands = commands
-    .slice(1)
-    .concat(helpCommand)
-    .map(x => [
-      { name: ZERO_WIDTH_SPACE, value: x.name, inline: true },
-      { name: ZERO_WIDTH_SPACE, value: x.usage, inline: true },
-      { name: ZERO_WIDTH_SPACE, value: x.description, inline: true }
-    ])
-    .flat();
+  const allCommands = commands.concat(helpCommand);
+
+  const fields = allCommands.reduce((fields, command) => {
+    fields[0].value += `${ command.name }\n`;
+    fields[1].value += `${ command.usage }\n`;
+    fields[2].value += `${ command.description }\n`;
+
+    return fields;
+  }, [
+    { name: 'Command', value: '', inline: true },
+    { name: 'Usage', value: '', inline: true },
+    { name: 'Description', value: '', inline: true }
+  ]);
 
   helpMessage = new Discord.MessageEmbed({
     title: 'Available commands',
-    fields: [
-      { name: 'Command', value: firstCommand.name, inline: true },
-      { name: 'Usage', value: firstCommand.usage, inline: true },
-      { name: 'Description', value: firstCommand.description, inline: true },
-      ...otherCommands
-    ]
+    fields
   });
 
   return helpCommand;
